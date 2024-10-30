4c63bb3eb80c756bab2bb2c957e68d72a9ff9a9a
muhammad hasbi ashiddiqi <muhammadhasbiasidiki175@gmail.com>
2024-10-31 02:23:43 +0700

    first commit

---
+++ ArrayBilangan17.java
@@ -0,0 +1,17 @@
+/**
+ * ArrayBilangan17
+ */
+public class ArrayBilangan17 {
+
+    public static void main(String[] args) {
+        double [] bill = new double[4];
+        bill[0] =  5.0;
+        bill[1] = 12867;
+        bill[2] = 7.5;
+        bill[3] = 2000000;
+        System.out.println(bill[0]);
+        System.out.println(bill[1]);
+        System.out.println(bill[2]);
+        System.out.println(bill[3]);
+    }
+}
