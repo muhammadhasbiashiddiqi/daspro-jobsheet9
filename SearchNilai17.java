b25f675e14ada2d970acd3954028bd7bf4f2eb0e
muhammad hasbi ashiddiqi <muhammadhasbiasidiki175@gmail.com>
2024-10-31 09:46:35 +0700

    first commit

--- SearchNilai17.java
+++ SearchNilai17.java
@@ -1,26 +1,33 @@
+import java.util.Scanner;
+
 /**
  * SearchNilai17
  */
 public class SearchNilai17 {
 
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = -1;
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int jumlahNilai = sc.nextInt();
+        int[] arrNilai = new int[jumlahNilai];
         for (int i = 0; i < arrNilai.length; i++) {
-            if (key == arrNilai[i]) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            arrNilai[i] = sc.nextInt();
+        }
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = sc.nextInt();
+        int hasil = -1;
+        for (int i = 0; i < arrNilai.length; i++) {
+            if (arrNilai[i] == key) {
                 hasil = i;
                 break;
             }
         }
-
         System.out.println();
         if (hasil != -1) {
-            System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1));
         } else {
             System.out.println("Nilai " + key + " tidak ditemukan di array.");
         }
-        System.out.println();
     }
 }
